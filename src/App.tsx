75c462156d079ed9d6d7566aeadb3d069acb28fd
Mikhail Nikonov <lounge1k@gmail.com>
2018-10-16 09:25:58 +0200

    strcuture

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import './App.css';
 import Paginator from './components/Paginator.component';
 
 class App extends React.Component {
+  public tabs:string[] = [
+    'tab1',
+    'tab2',
+    'tab3',
+    'tab1',
+    'tab2',
+    'tab3'
+  ]
+
   public render() {
     return (
       <div className="App">
-          <Paginator />
+          <Paginator tabs={this.tabs}/>
       </div>
     );
   }
--- src/components/Paginator.component.tsx
+++ src/components/Paginator.component.tsx
@@ -2,50 +2,70 @@ import * as React from 'react';
 
 import './Paginator.component.css';
 
-interface IMyState {
-    activeElement: number
+interface IProps {
+  tabs: string[]
 }
 
-class Paginator extends React.Component<{},IMyState> {
+interface IState {
+  activeElement: number
+}
 
-    private list:string[] = [
-        'tab1',
-        'tab2',
-        'tab3'
-    ];
+class Paginator extends React.Component<IProps, IState> {
 
-    constructor(props:any) {
-        super(props);
-        this.state = {
-            activeElement: 0
-        }
-    }
+  public readonly state: IState = {
+    activeElement: 0
+  }
 
-   public render() {
-        return(
-            <div className="paginator">
-                <div className="row ">
-                    <div className="col-1 ">
-                        <button>{'<'}</button>
-                    </div>
-                    <div className="col-fill ">
-                        <div className="row">
-                            {this.list.map((tabName:string, index:number) => {
-                               return(
-                                    <div key={index} className="col-fill">
-                                        <button className={this.state.activeElement === index ? 'btn-secondary' : ''}>{tabName}</button>
-                                    </div>
-                               )
-                            })}
-                        </div>
-                    </div>
-                    <div className="col-1 ">
-                        <button>{'>'}</button>
-                    </div>
-                </div>
+  public render() {
+    return (
+      <div className="paginator">
+        <div className="row flex-edges child-borders">
+          <div className="col-1 col">
+            {/* <button onClick={this.prevTab}></button> */}
+            <span className="border border-primary">{'<'}</span>
+          </div>
+          <div className="col-fill col">
+            <div className="row">
+              {this.props.tabs.map((tabName: string, index: number) => {
+                return (
+                  <div 
+                    key={index}
+                    className="col-fill col">
+                    <button
+                      onClick={this.selectTab(index)}
+                      className={this.state.activeElement === index ? 'btn-secondary' : ''}>{tabName}
+                    </button>
+                  </div>
+                )
+              })}
             </div>
-        )
-   }
+          </div>
+          <div className="col-1 col">
+            {/* <button onClick={this.nextTab}></button> */}
+            <span className="border border-primary">{'>'}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  private selectTab = (index: number) => {
+    return () => this.setState({
+      activeElement: index
+    })
+  }
+
+  // private nextTab = () => {
+  //   this.setState({
+  //     activeElement: this.state.activeElement < this.props.tabs.length - 1 ? ++this.state.activeElement : 0
+  //   })
+  // }
+
+  // private prevTab = () => {
+  //   this.setState({
+  //     activeElement: this.state.activeElement > 0 ? --this.state.activeElement : this.props.tabs.length - 1
+  //   })
+  // }
 }
 
 export default Paginator;
